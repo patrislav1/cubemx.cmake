9f3886ae8e8159e42dc32da24f833c75ac388d80
Patrick Huesmann <patrick.huesmann@desy.de>
2021-08-27 12:19:59 +0200

    Remove pyocd-wrapper as cortex-debug has been fixed

--- cmake/pyocd/vscode-debug.cmake
+++ cmake/pyocd/vscode-debug.cmake
@@ -3,8 +3,6 @@ set(PYOCD_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
 function(vscode_debug PROJ_NAME)
     set(PROJ_ELF_PATH "${CMAKE_BINARY_DIR}/${PROJ_NAME}.elf")
-    # pyocd-wrapper necessary b/c of Cortex-Debug issues
-    set(PYOCD_WRAPPER "${PYOCD_DIR}/pyocd-wrapper.py")
     file(MAKE_DIRECTORY "${VSCODE_DIR}")
     configure_file(
         "${PYOCD_DIR}/vscode-debug.in"
